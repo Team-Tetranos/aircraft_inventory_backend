3ba1559700e1064e4b8d7820c16f715664de93cf
Sifat-16 <sifatalamcep16@gmail.com>
2023-07-31 12:13:02 +0600

    massive update

--- aircraft_inventory_system/account/models.py
+++ aircraft_inventory_system/account/models.py
@@ -8,14 +8,13 @@ from .manager import CustomUserManager
 # Create your models here.
 
 class User(AbstractBaseUser, PermissionsMixin):
-    id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(db_index=True, unique=True, max_length=25)
     is_verified = models.BooleanField(default=False)
     otp = models.CharField(max_length=6, null=True, blank=True)
     is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-
 
     objects = CustomUserManager()
 
@@ -24,8 +23,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
-
-
-
-
-
