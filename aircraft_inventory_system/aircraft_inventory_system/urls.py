627ba62621094ccaab6123a0e88aa305190ebbb1
Sifat-16 <sifatalamcep16@gmail.com>
2023-08-02 05:26:48 +0600

    settings changed

--- aircraft_inventory_system/aircraft_inventory_system/urls.py
+++ aircraft_inventory_system/aircraft_inventory_system/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,4 @@ urlpatterns = [
     path('api/profile/', include('profiles.urls')),
     path('api/otp/', include('otp.urls')),
     path('api/aircraft/', include('aircraft.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
